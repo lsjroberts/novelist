293726aa1b610b8eda391bcbc15c582c026724e1
Laurence Roberts <lsjroberts@outlook.com>
2016-12-14 01:01:40 +0000

    Add electron app configuration

---
+++ app/electron.js
@@ -0,0 +1,78 @@
+'use strict';
+const { app, Menu, BrowserWindow } = require('electron');
+
+let mainWindow;
+
+
+
+// -- GLOBAL
+
+
+app.on('ready', createWindow);
+
+function createWindow() {
+    mainWindow = new BrowserWindow({
+        width: 1024,
+        height: 768,
+    });
+
+    Menu.setApplicationMenu(Menu.buildFromTemplate(menuTemplate));
+
+    mainWindow.loadURL(`file://${__dirname}/index.html`);
+
+    // mainWindow.webContents.openDevTools();
+
+    mainWindow.on('closed', () => {
+        mainWindow = null;
+    });
+}
+
+
+
+// -- OSX
+
+
+app.on('window-all-closed', () => {
+    if (process.platform !== 'darwin') {
+        app.quit();
+    }
+});
+
+app.on('activate', () => {
+    if (mainWindow === null) {
+        createWindow();
+    }
+});
+
+
+
+
+// -- MENU
+
+const menuTemplate = [
+    {
+        label: 'File',
+        submenu: [
+            { label: 'New Story', click() { console.log('File.New Story'); } },
+        ],
+    },
+];
+
+if (process.platform === 'darwin') {
+    menuTemplate.unshift({
+        label: app.getName(),
+        submenu: [
+            {
+                label: `About ${app.getName()}`,
+                role: 'about',
+            },
+            {
+                label: 'Quit',
+                accelerator: 'Command+Q',
+                click() {
+                    app.quit();
+                },
+            },
+        ],
+    });
+}
---
+++ app/ports.js
@@ -0,0 +1,5 @@
+'use strict';
+const Elm = require('./elm.js');
+
+const container = document.getElementById('container');
+const novelist = Elm.Main.embed(container);
