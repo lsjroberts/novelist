77ebabee511f61db493a43e54271546a37c641f2
Laurence Roberts <lsjroberts@outlook.com>
2017-06-06 23:27:11 +0100

    Add edit commands

--- app/electron.js
+++ app/electron.js
@@ -107,9 +107,23 @@ const menuTemplate = [
               click() {
                 saveProject();
               }
-            }
+            },
+            { type: "separator" },
+            { label: "Quit", accelerator: "Command+Q", click: function() { app.quit(); }}
         ],
     },
+    {
+      label: 'Edit',
+      submenu: [
+        { label: "Undo", accelerator: "CmdOrCtrl+Z", selector: "undo:" },
+        { label: "Redo", accelerator: "Shift+CmdOrCtrl+Z", selector: "redo:" },
+        { type: "separator" },
+        { label: "Cut", accelerator: "CmdOrCtrl+X", selector: "cut:" },
+        { label: "Copy", accelerator: "CmdOrCtrl+C", selector: "copy:" },
+        { label: "Paste", accelerator: "CmdOrCtrl+V", selector: "paste:" },
+        { label: "Select All", accelerator: "CmdOrCtrl+A", selector: "selectAll:" }
+      ]
+    }
 ];
 
 if (process.platform === 'darwin') {
