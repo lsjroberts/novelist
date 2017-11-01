dd3d2068812073b607eae3188772d679f3b1e1ff
Laurence Roberts <lsjroberts@outlook.com>
2017-11-01 23:21:23 +0100

    Remove old code

--- src/Utils/List.elm
+++
@@ -1,8 +0,0 @@
-module Utils.List exposing (..)
-
-
-getById : List { a | id : Int } -> Int -> Maybe { a | id : Int }
-getById xs id =
-    xs
-        |> List.filter (\x -> x.id == id)
-        |> List.head
