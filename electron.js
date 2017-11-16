2d26fb126db86fc2707abc135723292f79a68f46
Laurence Roberts <lsjroberts@outlook.com>
2017-11-16 18:35:00 +0100

    Remove old connect server

--- electron.js
+++ electron.js
@@ -2,12 +2,6 @@
 
 const { Menu, BrowserWindow, app, dialog } = require('electron');
 const path = require('path');
-const connect = require('connect');
-const serveStatic = require('serve-static');
-
-connect()
-    .use(serveStatic(__dirname))
-    .listen(8080);
 
 let windows = [];
 
